d79de2f51c0fbc5477f2a4e933f5e4751453883a
Dimitrios Karagiannis <dhkarag@gmail.com>
2019-09-09 16:49:25 +0100

    Add bits for updating the wireguard configuration
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

---
+++ wireguard.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"net"
+	"time"
+
+	"golang.zx2c4.com/wireguard/wgctrl"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+)
+
+const (
+	defaultPersistentKeepaliveInterval = 25 * time.Second
+	defaultDeviceName                  = "wg0"
+)
+
+func newPeerConfig(publicKey string, presharedKey string, endpoint string, allowedIPs []string) (*wgtypes.PeerConfig, error) {
+	key, err := wgtypes.ParseKey(publicKey)
+	if err != nil {
+		return nil, err
+	}
+	t := defaultPersistentKeepaliveInterval
+	peer := &wgtypes.PeerConfig{PublicKey: key, PersistentKeepaliveInterval: &t}
+	if presharedKey != "" {
+		key, err := wgtypes.ParseKey(presharedKey)
+		if err != nil {
+			return nil, err
+		}
+		peer.PresharedKey = &key
+	}
+	if endpoint != "" {
+		addr, err := net.ResolveUDPAddr("udp4", endpoint)
+		if err != nil {
+			return nil, err
+		}
+		peer.Endpoint = addr
+	}
+	for _, ai := range allowedIPs {
+		_, network, err := net.ParseCIDR(ai)
+		if err != nil {
+			return nil, err
+		}
+		peer.AllowedIPs = append(peer.AllowedIPs, *network)
+	}
+	return peer, nil
+}
+
+func setPeers(deviceName string, peers []wgtypes.PeerConfig) error {
+	wg, err := wgctrl.New()
+	if err != nil {
+		return err
+	}
+	defer wg.Close()
+	if deviceName == "" {
+		deviceName = defaultDeviceName
+	}
+	return wg.ConfigureDevice(deviceName, wgtypes.Config{ReplacePeers: true, Peers: peers})
+}
