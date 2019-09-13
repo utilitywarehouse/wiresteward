2b5bb7bcbccb53b91ab111e2de87ceb0cdbdc1b4
Dimitrios Karagiannis <dhkarag@gmail.com>
2019-09-13 14:34:47 +0100

    Rework http handlers
    - Add a new handler for /config
    - Handle POST requests to / for updating keys
    - Refactor bits of the code to make it more readable
    - Move initialisation and globals to main.go
    
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

--- http.go
+++ http.go
@@ -2,21 +2,17 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
 	"encoding/gob"
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 
 	"github.com/gofrs/uuid"
-	"github.com/gorilla/securecookie"
-	"github.com/gorilla/sessions"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	admin "google.golang.org/api/admin/directory/v1"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 	oauth2api "google.golang.org/api/oauth2/v2"
 )
 
@@ -56,181 +52,212 @@ const (
 </body>
 </html>`
 
-	tplUserConfigSource = ``
+	tplUserConfigSource = `[Interface]
+Address = {{.Address}}
+PrivateKey = <replace-with-your-private-key>
+{{ range .Peers }}
+[Peer]
+PublicKey = {{ .PublicKey }}
+Endpoint = {{ .Endpoint }}
+AllowedIPs = {{ .AllowedIPs }}
+PersistentKeepalive = 25
+{{ end }}`
+
+	genericErrorMessage = `<!DOCTYPE html>
+<html>
+<head>
+<title>wireguard-thingy</title>
+</head>
+<body>
+<div>
+<p>An error occurred, please try again.</p>
+<p>If this persists, try clearing your cookies for this domain.</p>
+<p><a href="/">Go to the main page.</a></p>
+</div>
+</body>
+</html>`
 )
 
 var (
-	tplMain, tplUserConfig *template.Template
-	gsuiteService          *admin.Service
-	oAuthConfig            *oauth2.Config
-	sessionStore           *sessions.CookieStore
+	errInvalidOrMissingToken = errors.New("Invalid or missing access token")
 
-	listenAddress                                = os.Getenv("WGS_LISTEN_ADDRESS")
-	googleClientID                               = os.Getenv("WGS_CLIENT_ID")
-	googleClientSecret                           = os.Getenv("WGS_CLIENT_SECRET")
-	googleCallbackURL                            = os.Getenv("WGS_CALLBACK_URL")
-	googleAdminEmail                             = os.Getenv("WGS_ADMIN_EMAIL")
-	googleServiceAccountKeyPath                  = os.Getenv("WGS_SERVICE_ACCOUNT_KEY_PATH")
-	allowedGoogleGroups                          = strings.Split(os.Getenv("WGS_ALLOWED_GOOGLE_GROUPS"), ",")
-	cookieAuthenticationKey, cookieEncryptionKey []byte
+	tplMain, tplUserConfig *template.Template
 )
 
 func init() {
+	gob.Register(&oauth2.Token{}) // So we can save the token in a cookie
 	tplMain = template.Must(template.New("main").Parse(tplMainSource))
 	tplUserConfig = template.Must(template.New("userConfig").Parse(tplUserConfigSource))
 }
 
-func initServer() {
-	var err error
-	gob.Register(&oauth2.Token{}) // So we can save the token in a cookie
-	if listenAddress == "" {
-		listenAddress = defaultListenAddress
-	}
-	if googleCallbackURL == "" {
-		log.Fatal("Environment variable WGS_CALLBACK_URL is not set")
-	}
-	if googleClientID == "" {
-		log.Fatal("Environment variable WGS_CLIENT_ID is not set")
-	}
-	if googleClientSecret == "" {
-		log.Fatal("Environment variable WGS_CLIENT_SECRET is not set")
-	}
-	if googleAdminEmail == "" {
-		log.Fatal("Environment variable WGS_ADMIN_EMAIL is not set")
-	}
-	if googleServiceAccountKeyPath == "" {
-		log.Fatal("Environment variable WGS_SERVICE_ACCOUNT_KEY_PATH is not set")
-	}
-	if cs := os.Getenv("WGS_COOKIE_AUTHENTICATION_KEY"); cs == "" {
-		log.Print("Environment variable WGS_COOKIE_AUTHENTICATION_KEY is not set, will generate a temporary key")
-		cookieAuthenticationKey = securecookie.GenerateRandomKey(64)
-	} else {
-		cookieAuthenticationKey, err = base64.StdEncoding.DecodeString(cs)
-		if err != nil {
-			log.Fatalf("Could not decode cookie authentication key: %v", err)
-		}
-	}
-	if cs := os.Getenv("WGS_COOKIE_ENCRYPTION_KEY"); cs == "" {
-		log.Print("Environment variable WGS_COOKIE_ENCRYPTION_KEY is not set, will generate a temporary key")
-		cookieEncryptionKey = securecookie.GenerateRandomKey(32)
-	} else {
-		cookieEncryptionKey, err = base64.StdEncoding.DecodeString(cs)
-		if err != nil {
-			log.Fatalf("Could not decode cookie encryption key: %v", err)
-		}
-	}
-	oAuthConfig = &oauth2.Config{
-		ClientID:     googleClientID,
-		ClientSecret: googleClientSecret,
-		RedirectURL:  googleCallbackURL,
-		Scopes:       []string{"email", "profile"},
-		Endpoint:     google.Endpoint,
-	}
-	sessionStore = sessions.NewCookieStore(cookieAuthenticationKey, cookieEncryptionKey)
-	sessionStore.MaxAge(defaultSessionDuration)
-	sessionStore.Options = &sessions.Options{HttpOnly: true}
-	svc, err = newDirectoryService(
-		context.Background(),
-		googleServiceAccountKeyPath,
-		googleAdminEmail,
-		admin.AdminDirectoryUserschemaScope,
-		admin.AdminDirectoryGroupMemberReadonlyScope,
-	)
-	if err != nil {
-		log.Fatalf("Could not initialise google client: %v", err)
-	}
-	if err := ensureGSuiteCustomSchema(svc); err != nil {
-		log.Fatalf("Could not setup custom user schema: %v", err)
-	}
-}
-
-func mainHandler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		session, err := sessionStore.Get(r, "_wgs")
-		if err != nil {
-			if err := redirectToLogin(w, r); err != nil {
-				reportError(w, err)
-			}
-			return
-		}
-		tok, ok := session.Values["token"].(*oauth2.Token)
-		if !ok || !tok.Valid() {
-			if err := redirectToLogin(w, r); err != nil {
-				reportError(w, err)
-			}
-			return
-		}
-		ctx := context.Background()
-		ui, err := oauth2api.New(oauth2.NewClient(ctx, oAuthConfig.TokenSource(ctx, tok)))
-		if err != nil {
-			fmt.Fprintf(w, "could not get userinfo client: %v", err)
-			return
-		}
-		uip, err := ui.Userinfo.V2.Me.Get().Do()
-		if err != nil {
-			fmt.Fprintf(w, "could not get userinfo: %v", err)
-			return
-		}
-		ugs, err := newPeerConfigFromGoogle(svc, uip.Email)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		allowedIPs := make([]string, len(ugs.AllowedIPs))
-		for i, v := range ugs.AllowedIPs {
-			allowedIPs[i] = v.String()
-		}
-		if err := tplMain.Execute(w, map[string]string{
-			"Email":      uip.Email,
-			"PublicKey":  ugs.PublicKey.String(),
-			"AllowedIPs": strings.Join(allowedIPs, ","),
-		}); err != nil {
-			log.Fatalln(err)
-		}
-	})
-}
-
 func callbackHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			plainHTTPError(w, http.StatusBadRequest)
+			return
+		}
 		oauthSession, err := sessionStore.Get(r, r.FormValue("state"))
 		if err != nil {
-			reportError(w, fmt.Errorf("could not retrieve oauth session: %w", err))
+			reportFatalError(w, fmt.Errorf("could not retrieve oauth session: %w", err))
 			return
 		}
 		oauthSession.Options.MaxAge = -1 // deletes the session
 		if err := oauthSession.Save(r, w); err != nil {
-			reportError(w, fmt.Errorf("could not delete oauth session: %w", err))
+			reportFatalError(w, fmt.Errorf("could not delete oauth session: %w", err))
 			return
 		}
 		tok, err := oAuthConfig.Exchange(context.Background(), r.FormValue("code"))
 		if err != nil {
-			reportError(w, fmt.Errorf("could not exchange code for token: %w", err))
+			reportFatalError(w, fmt.Errorf("could not exchange code for token: %w", err))
 			return
 		}
-		session, err := sessionStore.New(r, "_wgs")
+		session, err := sessionStore.New(r, defaultSessionKey)
 		if err != nil {
-			reportError(w, fmt.Errorf("could not create user session: %w", err))
+			reportFatalError(w, fmt.Errorf("could not create user session: %w", err))
 			return
 		}
 		session.Values["token"] = tok
 		if err := session.Save(r, w); err != nil {
-			reportError(w, fmt.Errorf("could not save user session: %w", err))
+			reportFatalError(w, fmt.Errorf("could not save user session: %w", err))
 			return
 		}
 		if redirectURL, ok := oauthSession.Values["redirect_to"].(string); ok {
 			http.Redirect(w, r, redirectURL, http.StatusFound)
+		} else {
+			fmt.Fprintf(w, "You have successfully authenticated")
 		}
 	})
 }
 
-func reportError(w http.ResponseWriter, err error) {
+func mainHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			plainHTTPError(w, http.StatusNotFound)
+			return
+		}
+		email, err := authenticateUser(r)
+		if err != nil {
+			reportRedirectError(w, r, err)
+			return
+		}
+		var peer *wgtypes.PeerConfig
+		switch r.Method {
+		case http.MethodPost:
+			peer, err = newPeerConfig(r.FormValue("publicKey"), "", "", strings.Split(r.FormValue("allowedIPs"), ","))
+			if err != nil {
+				reportFatalError(w, err)
+				return
+			}
+			peer, err = updatePeerConfigInGsuite(gsuiteService, email, peer)
+			if err != nil {
+				reportFatalError(w, err)
+				return
+			}
+		case http.MethodGet:
+			peer, err = getPeerConfigFromGsuite(gsuiteService, email)
+			if err != nil {
+				reportFatalError(w, err)
+				return
+			}
+		default:
+			plainHTTPError(w, http.StatusBadRequest)
+			return
+		}
+		allowedIPs := make([]string, len(peer.AllowedIPs))
+		for i, v := range peer.AllowedIPs {
+			allowedIPs[i] = v.String()
+		}
+		if err := tplMain.Execute(w, map[string]string{
+			"Email":      email,
+			"PublicKey":  peer.PublicKey.String(),
+			"AllowedIPs": strings.Join(allowedIPs, ","),
+		}); err != nil {
+			reportFatalError(w, err)
+			return
+		}
+	})
+}
+
+func configHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			plainHTTPError(w, http.StatusBadRequest)
+			return
+		}
+		email, err := authenticateUser(r)
+		if err != nil {
+			reportRedirectError(w, r, err)
+			return
+		}
+		peer, err := getPeerConfigFromGsuite(gsuiteService, email)
+		if err != nil {
+			reportFatalError(w, err)
+			return
+		}
+		allowedIPs := make([]string, len(peer.AllowedIPs))
+		for i, v := range peer.AllowedIPs {
+			allowedIPs[i] = v.String()
+		}
+		if err := tplUserConfig.Execute(w, map[string]interface{}{
+			"Address": strings.Join(allowedIPs, ","),
+			"Peers":   serverPeers,
+		}); err != nil {
+			reportFatalError(w, err)
+			return
+		}
+	})
+}
+
+func authenticateUser(r *http.Request) (string, error) {
+	tok, err := extractToken(r)
 	if err != nil {
-		log.Printf("user flow error: %v", err)
+		return "", err
 	}
-	http.Error(w,
-		`An error occurred, please try logging in again.
-If this persists, try clearing your cookies for this domain`,
-		http.StatusInternalServerError,
-	)
+	return getUserEmailFromToken(tok)
+}
+
+func extractToken(r *http.Request) (*oauth2.Token, error) {
+	session, err := sessionStore.Get(r, defaultSessionKey)
+	if err != nil {
+		return nil, err
+	}
+	tok, ok := session.Values["token"].(*oauth2.Token)
+	if !ok || !tok.Valid() {
+		return nil, errInvalidOrMissingToken
+	}
+	return tok, nil
+}
+
+func getUserEmailFromToken(tok *oauth2.Token) (string, error) {
+	ctx := context.Background()
+	ui, err := oauth2api.New(oauth2.NewClient(ctx, oAuthConfig.TokenSource(ctx, tok)))
+	if err != nil {
+		return "", err
+	}
+	user, err := ui.Userinfo.V2.Me.Get().Do()
+	if err != nil {
+		return "", err
+	}
+	return user.Email, nil
+}
+
+func plainHTTPError(w http.ResponseWriter, code int) {
+	http.Error(w, http.StatusText(code), code)
+}
+
+func reportRedirectError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("user flow error: %v", err)
+	if err := redirectToLogin(w, r); err != nil {
+		reportFatalError(w, err)
+	}
+}
+
+func reportFatalError(w http.ResponseWriter, err error) {
+	log.Printf("user flow error: %v", err)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprintln(w, genericErrorMessage)
 }
 
 func redirectToLogin(w http.ResponseWriter, r *http.Request) error {
--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"encoding/base64"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
 	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	admin "google.golang.org/api/admin/directory/v1"
 )
 
 const (
 	usage = `usage: wireguard-thing (server|agent)`
+)
+
+var (
+	gsuiteService *admin.Service
+	oAuthConfig   *oauth2.Config
+	sessionStore  *sessions.CookieStore
+
+	listenAddress                                = os.Getenv("WGS_LISTEN_ADDRESS")
+	googleClientID                               = os.Getenv("WGS_CLIENT_ID")
+	googleClientSecret                           = os.Getenv("WGS_CLIENT_SECRET")
+	googleCallbackURL                            = os.Getenv("WGS_CALLBACK_URL")
+	googleAdminEmail                             = os.Getenv("WGS_ADMIN_EMAIL")
+	googleServiceAccountKeyPath                  = os.Getenv("WGS_SERVICE_ACCOUNT_KEY_PATH")
+	allowedGoogleGroups                          = strings.Split(os.Getenv("WGS_ALLOWED_GOOGLE_GROUPS"), ",")
+	cookieAuthenticationKey, cookieEncryptionKey []byte
 )
 
 func main() {
@@ -28,9 +49,75 @@ func main() {
 	}
 }
 
+func initServer() {
+	var err error
+	if listenAddress == "" {
+		listenAddress = defaultListenAddress
+	}
+	if googleCallbackURL == "" {
+		log.Fatal("Environment variable WGS_CALLBACK_URL is not set")
+	}
+	if googleClientID == "" {
+		log.Fatal("Environment variable WGS_CLIENT_ID is not set")
+	}
+	if googleClientSecret == "" {
+		log.Fatal("Environment variable WGS_CLIENT_SECRET is not set")
+	}
+	if googleAdminEmail == "" {
+		log.Fatal("Environment variable WGS_ADMIN_EMAIL is not set")
+	}
+	if googleServiceAccountKeyPath == "" {
+		log.Fatal("Environment variable WGS_SERVICE_ACCOUNT_KEY_PATH is not set")
+	}
+	if cs := os.Getenv("WGS_COOKIE_AUTHENTICATION_KEY"); cs == "" {
+		log.Print("Environment variable WGS_COOKIE_AUTHENTICATION_KEY is not set, will generate a temporary key")
+		cookieAuthenticationKey = securecookie.GenerateRandomKey(64)
+	} else {
+		cookieAuthenticationKey, err = base64.StdEncoding.DecodeString(cs)
+		if err != nil {
+			log.Fatalf("Could not decode cookie authentication key: %v", err)
+		}
+	}
+	if cs := os.Getenv("WGS_COOKIE_ENCRYPTION_KEY"); cs == "" {
+		log.Print("Environment variable WGS_COOKIE_ENCRYPTION_KEY is not set, will generate a temporary key")
+		cookieEncryptionKey = securecookie.GenerateRandomKey(32)
+	} else {
+		cookieEncryptionKey, err = base64.StdEncoding.DecodeString(cs)
+		if err != nil {
+			log.Fatalf("Could not decode cookie encryption key: %v", err)
+		}
+	}
+	oAuthConfig = &oauth2.Config{
+		ClientID:     googleClientID,
+		ClientSecret: googleClientSecret,
+		RedirectURL:  googleCallbackURL,
+		Scopes:       []string{"email", "profile"},
+		Endpoint:     google.Endpoint,
+	}
+	sessionStore = sessions.NewCookieStore(cookieAuthenticationKey, cookieEncryptionKey)
+	sessionStore.MaxAge(defaultSessionDuration)
+	//sessionStore.Options = &sessions.Options{Secure: true, HttpOnly: true} // XXX
+	sessionStore.Options = &sessions.Options{HttpOnly: true}
+	gsuiteService, err = newDirectoryService(
+		context.Background(),
+		googleServiceAccountKeyPath,
+		googleAdminEmail,
+		admin.AdminDirectoryUserschemaScope,
+		admin.AdminDirectoryUserScope,
+		admin.AdminDirectoryGroupMemberReadonlyScope,
+	)
+	if err != nil {
+		log.Fatalf("Could not initialise google client: %v", err)
+	}
+	if err := ensureGSuiteCustomSchema(gsuiteService); err != nil {
+		log.Fatalf("Could not setup custom user schema: %v", err)
+	}
+}
+
 func serve() {
 	initServer()
 	m := http.NewServeMux()
+	m.Handle("/config", configHandler())
 	m.Handle("/callback", callbackHandler())
 	m.Handle("/", mainHandler())
 	http.Handle("/", m)
