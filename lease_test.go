e99573e1904082f75ed9341732f9bf96844a3f7c
Foivos Filippopoulos <ffilippopoulos@utilitywarehouse.co.uk>
2020-07-31 14:27:34 +0100

    squeeze to avoid cringe, and rename

--- lease_test.go
+++ lease_test.go
@@ -5,22 +5,18 @@ import (
 	"testing"
 )
 
-func TestFindNextAvailableIpAddress(t *testing.T) {
-
+func TestFileLeaseManager_FindNextAvailableIpAddress(t *testing.T) {
 	// Test that lm.ip is skipped
 	ip, network, _ := net.ParseCIDR("10.90.0.1/20")
 	test_lm := &FileLeaseManager{
 		cidr: network,
 		ip:   ip,
 	}
-
 	aip, err := test_lm.findNextAvailableIpAddress()
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	if !aip.IP.Equal(net.ParseIP("10.90.0.2")) {
 		t.Fatalf("Unexpected ip returned %v", aip.IP)
 	}
-
 }
