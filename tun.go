c4d211e73445453192a82e917935124423045a1d
Dimitrios Karagiannis <dhkarag@gmail.com>
2020-08-03 11:17:26 +0100

    Importove TunDevice cleanup
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

--- tun.go
+++ tun.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 
 	"golang.zx2c4.com/wireguard/device"
 	"golang.zx2c4.com/wireguard/ipc"
@@ -10,18 +11,19 @@ import (
 )
 
 type TunDevice struct {
-	tun        tun.Device
 	device     *device.Device
 	deviceName string
+	errs       chan error
 	logger     *device.Logger
 	uapi       net.Listener
-	errs       chan error
+	uapiSocket *os.File
 	stop       chan bool
 	stopped    chan bool
+	tunDevice  tun.Device
 }
 
 func startTunDevice(name string) (*TunDevice, error) {
-	tun, err := tun.CreateTUN(name, device.DefaultMTU)
+	tunDevice, err := tun.CreateTUN(name, device.DefaultMTU)
 	if err != nil {
 		return &TunDevice{}, fmt.Errorf("Cannot create tun device %v", err)
 	}
@@ -31,25 +33,26 @@ func startTunDevice(name string) (*TunDevice, error) {
 		fmt.Sprintf("(%s) ", name),
 	)
 
-	device := device.NewDevice(tun, logger)
+	device := device.NewDevice(tunDevice, logger)
 	logger.Info.Println("Device started")
 
-	fileUAPI, err := ipc.UAPIOpen(name)
+	uapiSocket, err := ipc.UAPIOpen(name)
 	if err != nil {
 		return &TunDevice{}, fmt.Errorf("Failed to open uapi socket file: %v", err)
 	}
 
-	uapi, err := ipc.UAPIListen(name, fileUAPI)
+	uapi, err := ipc.UAPIListen(name, uapiSocket)
 	if err != nil {
 		return &TunDevice{}, fmt.Errorf("Failed to listen on uapi socket: %v", err)
 	}
 	tundev := &TunDevice{
-		tun:        tun,
 		device:     device,
 		deviceName: name,
+		errs:       make(chan error),
 		logger:     logger,
 		uapi:       uapi,
-		errs:       make(chan error),
+		uapiSocket: uapiSocket,
+		tunDevice:  tunDevice,
 	}
 
 	return tundev, nil
@@ -81,7 +84,7 @@ func (td *TunDevice) Run() {
 	case <-td.device.Wait():
 	}
 
-	td.CleanUp()
+	td.cleanup()
 	close(td.stopped)
 }
 
@@ -90,9 +93,11 @@ func (td *TunDevice) Stop() {
 	<-td.stopped
 }
 
-func (td *TunDevice) CleanUp() {
-	td.uapi.Close()
-	td.device.Close()
-
+func (td *TunDevice) cleanup() {
 	td.logger.Info.Println("Shutting down")
+	td.uapi.Close()
+	td.logger.Info.Println("UAPI listener stopped")
+	td.uapiSocket.Close()
+	td.device.Close()
+	td.tunDevice.Close()
 }
