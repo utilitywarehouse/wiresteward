c10809b94100e550c51442e946d76f726abbcfb2
Dimitrios Karagiannis <dhkarag@gmail.com>
2020-08-06 12:02:23 +0100

    Remove unused global variable
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"log"
-	"net"
 	"os"
 	"os/signal"
 	"syscall"
@@ -16,7 +15,6 @@ const (
 )
 
 var (
-	userPeerSubnet     *net.IPNet
 	leaserSyncInterval time.Duration
 
 	flagAgent   = flag.Bool("agent", false, "Run application in \"agent\" mode")
