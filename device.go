840c1229efcffac66c243818e7c7215e121a2ca4
Dimitrios Karagiannis <dhkarag@gmail.com>
2020-08-20 10:55:25 +0100

    Call LinkSetUp after timeout
    In 97d8804d6d22b19f6213b1e30d787aa8b0521882 waitLinkIsUp was introduced
    to address an issue with the link automatically transitioning to the UP
    state. The issue described there was originally observed on flatcar
    linux.
    
    After further investigation, it appears that the behaviour described in
    the commit above is not consistent across different systems. In Debian,
    for example, after LinkAdd is called the link will remain in the DOWN
    state and waitLinkIsUp will timeout.
    
    Calling LinkSetUp before waitLinkIsUp will work in Debian but in flatcar
    it creates a scenario where the link has the UP flag set but AddrAdd
    still fails.
    
    Instead, we can resort to calling LinkSetUp only when we timeout waiting
    for the link to come up automatically.
    
    This diverges from the order in which wg-quick sets up things but there
    shouldn't be any complications arising from bringing the link up earlier
    in the process and setting it up afterwards.
    
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

--- device.go
+++ device.go
@@ -202,16 +202,13 @@ func (wd *WireguardDevice) Start() error {
 	if err := h.LinkAdd(wd.link); err != nil {
 		return err
 	}
+	if err := wd.ensureLinkIsUp(h); err != nil {
+		return err
+	}
 	if err := wd.configureWireguard(); err != nil {
 		return err
 	}
-	if err := wd.waitLinkIsUp(h); err != nil {
-		return err
-	}
 	if err := h.AddrAdd(wd.link, &wd.deviceAddress); err != nil {
-		return err
-	}
-	if err := h.LinkSetUp(wd.link); err != nil {
 		return err
 	}
 	log.Printf("Initialised device %s", wd.deviceName)
@@ -280,7 +277,13 @@ func (wd *WireguardDevice) configureWireguard() error {
 	})
 }
 
-func (wd *WireguardDevice) waitLinkIsUp(h netlink.Handle) error {
+// In Flatcar linux, the link automatically transitions to the UP state. In
+// Debian, the link will stay in the DOWN state until LinkSetUp is called.
+// Additionally, if LinkSetUp is called in Flatcar, the link appears to properly
+// get the UP flag set but subsequent AddrAdd() calls might fail, indicating
+// it did not properly set up. This method, will wait for the link to come up
+// automatically and will explicitly bring it up after timeout.
+func (wd *WireguardDevice) ensureLinkIsUp(h netlink.Handle) error {
 	tries := 1
 	for {
 		link, err := h.LinkByName(wd.deviceName)
@@ -289,10 +292,12 @@ func (wd *WireguardDevice) waitLinkIsUp(h netlink.Handle) error {
 		}
 		log.Printf("waiting for device %s to come up, current flags: %s", wd.deviceName, link.Attrs().Flags)
 		if link.Attrs().Flags&net.FlagUp != 0 {
+			log.Printf("device %s came up automatically", wd.deviceName)
 			return nil
 		}
-		if tries > 9 {
-			return fmt.Errorf("timeout waiting for device %s to come up", wd.deviceName)
+		if tries > 4 {
+			log.Printf("timeout waiting for device %s to come up automatically", wd.deviceName)
+			return h.LinkSetUp(wd.link)
 		}
 		tries++
 		time.Sleep(time.Second)
