a833a67552b1b334eb2c63eea9f8cf429bb6577c
Dimitrios Karagiannis <dhkarag@gmail.com>
2020-08-24 13:18:25 +0100

    terraform: add gcp module
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

---
+++ terraform/gcp/io.tf
@@ -0,0 +1,44 @@
+variable "role_name" {
+  type        = string
+  default     = "wiresteward"
+  description = "An identifier for the resources created by this module."
+}
+
+variable "dns_zone" {
+  type        = string
+  description = "The GCP managed zone in which records will be created"
+}
+
+variable "ignition" {
+  type        = list(string)
+  description = "The ignition configuration for the wiresteward instances. The length of this list determines the number of instances launched. Output of the ignition module."
+}
+
+variable "subnet_link" {
+  type        = string
+  description = "GCP subnet link in which the wiresteward instances will be deployed"
+}
+
+variable "vpc_link" {
+  type        = string
+  description = "The link of the GCP VPC in which to deploy the wiresteward instance."
+}
+
+variable "wireguard_endpoints" {
+  type        = list(string)
+  description = "A list of wireguard endpoints for the instances. Output of the ignition module."
+}
+
+variable "wiresteward_endpoint" {
+  type        = string
+  description = "The endpoint for wiresteward where clients connect."
+}
+
+locals {
+  instance_count = length(var.ignition)
+  name           = "${var.role_name}"
+}
+
+output "public_ipv4_addresses" {
+  value = [google_compute_instance.wiresteward.*.network_interface.0.access_config.0.nat_ip]
+}
