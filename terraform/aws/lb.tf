7ec65068c154d109216302b40c19ced6a547206c
George Angel <github-7574494a@infosecproject.org>
2020-09-17 12:15:19 +0100

    Convert validation_options to a list
    Keep it compatible by being able to keep route53 records inside the
    module

--- terraform/aws/lb.tf
+++ terraform/aws/lb.tf
@@ -38,11 +38,15 @@ resource "aws_acm_certificate" "cert" {
   validation_method = "DNS"
 }
 
+locals {
+  validation_options_list = tolist(aws_acm_certificate.cert.domain_validation_options)
+}
+
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+  name    = validation_options_list.0.resource_record_name
+  type    = validation_options_list.0.resource_record_type
   zone_id = var.dns_zone_id
-  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
+  records = [validation_options_list.0.resource_record_value]
   ttl     = 60
 }
 
