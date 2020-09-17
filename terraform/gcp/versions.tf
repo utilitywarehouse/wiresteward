5d8ed658d0a89b7b89b9832971a66e6bd243bcb4
George Angel <github-7574494a@infosecproject.org>
2020-09-17 09:56:53 +0100

    Upgrade terraform modules to 0.13

---
+++ terraform/gcp/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+  required_version = ">= 0.13"
+}
---
+++ terraform/ignition/versions.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    ignition = {
+      source = "terraform-providers/ignition"
+    }
+  }
+  required_version = ">= 0.13"
+}
