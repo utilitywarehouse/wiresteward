16ec60e1262eda86fa1385d1085d9ec6c3c8aa1e
Foivos Filippopoulos <foivosfilip@gmail.com>
2021-01-12 09:12:09 +0000

    Change metrics server port

--- metrics.go
+++ metrics.go
@@ -189,16 +189,17 @@ func (c *collector) getUserFromPubKey(pub string) string {
 	return ""
 }
 
-// Run metrics server on port 9586 that is marked for "WireGuard exporter":
+// Run metrics server on port 9781 which is the first available withing the
+// ranges defined here:
 // https://github.com/prometheus/prometheus/wiki/Default-port-allocations
 func startMetricsServer() {
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", promhttp.Handler())
 	server := http.Server{
-		Addr:    ":9586",
+		Addr:    ":9781",
 		Handler: mux,
 	}
-	logger.Info.Printf("Starting metrics server at :9586\n")
+	logger.Info.Printf("Starting metrics server at :9781\n")
 	if err := server.ListenAndServe(); err != nil {
 		logger.Error.Fatal(err)
 	}
