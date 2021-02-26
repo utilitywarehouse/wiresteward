39f64f4896d82ed6a421bf6946e1d791468a6814
Foivos Filippopoulos <foivosfilip@gmail.com>
2021-02-26 13:54:36 +0000

    Prepare terraform form aws/gcp v0.2.0
    - Change firewall to allow public traffic on http ports to reach traefik
    - Remove the local server listen address from the config to allow both the old
      and new setups to work. This should be re-added when we delete the load
      balancers in front of the instances.

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -29,6 +29,20 @@ resource "aws_security_group" "wiresteward" {
   }
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 51820
     to_port     = 51820
     protocol    = "udp"
